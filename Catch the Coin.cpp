4102008533707f780f7b86de650d8f8d09670930
NIDHI UPMAN <130860182+nidhiupman568@users.noreply.github.com>
2024-06-28 14:01:07 +0530

    Create Catch the Coin.cpp

---
+++ Catch the Coin.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define sq(a) (a)*(a)
+#define dbg(x) cout << #x << " = " << x << "\n"
+#define int long long
+ 
+ 
+void solve(){
+    int x,y; cin>>x>>y;
+    if(y>=-1) cout<<"YES\n"; else cout<<"NO\n";
+ 
+}
+ 
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+ 
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
